35130fd3e85c77c4838d44ffe4ff05378e459e1b
isuru-c <isuru.13@cse.mrt.ac.lk>
2016-01-06 21:54:48 +0530

    Add sport function implemented for the admin user

--- src/AppBundle/Controller/Sport/SportController.php
+++ src/AppBundle/Controller/Sport/SportController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller\Sport;
 
+use AppBundle\Entity\Sport;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class SportController extends Controller
 {
@@ -13,7 +15,51 @@ class SportController extends Controller
      */
     public function sportHomeAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('sport/home.html.twig', array());
+    }
+
+    /**
+     * @Route("/sport/new", name="sport_new")
+     * @Security("has_role('ROLE_ADMIN')")
+     *
+     */
+    public function sportNewAction(Request $request)
+    {
+        $sport = new Sport();
+
+        $form = $this->createFormBuilder($sport)
+            ->add('name', 'text')
+            ->add('gender', 'choice', array('choices' => array('Male', 'Female')))
+            ->add('minimumAge', 'integer')
+            ->add('description', 'textarea')
+            ->add('save', 'submit', array('label' => 'Add new Sport'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isValid()){
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($sport);
+            $em->flush();
+
+            return $this->render('sport/new.html.twig');
+        }
+
+        return $this->render('sport/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/sport/all", name="sport_all")
+     *
+     */
+    public function sportAllAction(Request $request)
+    {
+        $sports = $this->getDoctrine()
+            ->getRepository('AppBundle:Sport')->findAll();
+
+        return $this->render('sport/view.html.twig', array());
     }
 }
