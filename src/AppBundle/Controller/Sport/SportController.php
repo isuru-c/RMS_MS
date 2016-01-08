7c66ebabb3206267f5db90be4437bd3c3cd15e6b
isuru-c <isuru.13@cse.mrt.ac.lk>
2016-01-09 01:06:37 +0530

    View single sport, edit sport basic functions added

--- src/AppBundle/Controller/Sport/SportController.php
+++ src/AppBundle/Controller/Sport/SportController.php
@@ -29,7 +29,7 @@ class SportController extends Controller
 
         $form = $this->createFormBuilder($sport)
             ->add('name', 'text')
-            ->add('gender', 'choice', array('choices' => array('Male', 'Female')))
+            ->add('gender', 'choice', array('choices' => array('Male' => 'Male', 'Female'=>'Female')))
             ->add('minimumAge', 'integer')
             ->add('description', 'textarea')
             ->add('save', 'submit', array('label' => 'Add new Sport'))
@@ -52,14 +52,72 @@ class SportController extends Controller
     }
 
     /**
-     * @Route("/sport/all", name="sport_all")
+     * @Route("/sport/view/{id}", defaults={"id" = 0}, name="sport_view")
      *
      */
-    public function sportAllAction(Request $request)
+    public function sportViewAction(Request $request, $id)
     {
-        $sports = $this->getDoctrine()
-            ->getRepository('AppBundle:Sport')->findAll();
+        if($id == 0) {
+            $sports = $this->getDoctrine()
+                ->getRepository('AppBundle:Sport')->findAll();
 
-        return $this->render('sport/view.html.twig', array());
+            return $this->render('sport/view.html.twig', array(
+                'sports' => $sports, 'coaches' => $this->getAllCoaches(),
+            ));
+        }
+
+        $sport = $this->getDoctrine()
+            ->getRepository('AppBundle:Sport')->find($id);
+
+        if(!$sport){
+            throw new \Doctrine\ORM\NoResultException;
+        }
+        else{
+            return $this->render('sport/view1.html.twig', array(
+                'sport' => $sport,
+            ));
+        }
+    }
+
+    /**
+     * @Route("/sport/edit/{id}", defaults={"id" = 0}, name="sport_edit")
+     *
+     */
+    public function sportEditAction(Request $request, $id)
+    {
+        $sport = $this->getDoctrine()
+            ->getRepository('AppBundle:Sport')->find($id);
+
+        if(!$sport){
+            throw new \Doctrine\ORM\NoResultException;
+        }
+
+        $form = $this->createFormBuilder($sport)
+            ->add('name', 'text')
+            ->add('gender', 'choice', array('choices' => array('Male' => 'Male', 'Female'=>'Female')))
+            ->add('minimumAge', 'integer')
+            ->add('description', 'textarea')
+            ->add('save', 'submit', array('label' => 'Update details'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isValid()){
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($sport);
+            $em->flush();
+
+            return $this->render('sport/edit.html.twig');
+        }
+
+        return $this->render('sport/edit.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+
+    private function getAllCoaches(){
+        return 'not yet implemented';
     }
 }
